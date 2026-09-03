d8131c73687535f40778b92ad5d371be9db317db
Oleksii Korniienko <olkornii@redhat.com>
2026-09-03 17:27:12 +0200

    testing 1
    Signed-off-by: Oleksii Korniienko <olkornii@redhat.com>

--- tests/e2e/utils/DriverHelper.ts
+++ tests/e2e/utils/DriverHelper.ts
@@ -107,6 +107,12 @@ export class DriverHelper {
 			} catch (err) {
 				if (i >= attempts - 1) {
 					Logger.error(`failed with exception, out of attempts - ${err}`);
+					try {
+						const alert: any = await this.driver.switchTo().alert();
+						Logger.error(`DEBUG: Alert is open during waitVisibility! Text: "${await alert.getText()}"`);
+					} catch (alertErr) {
+						Logger.error('DEBUG: No alert present during unexpected exception');
+					}
 					throw err;
 				}
 
@@ -125,12 +131,6 @@ export class DriverHelper {
 				}
 
 				Logger.error(`failed with an unexpected exception - ${err}`);
-				try {
-					const alert: any = await this.driver.switchTo().alert();
-					Logger.error(`DEBUG: Alert is open during waitVisibility! Text: "${await alert.getText()}"`);
-				} catch (alertErr) {
-					Logger.error('DEBUG: No alert present during unexpected exception');
-				}
 				throw err;
 			}
 
